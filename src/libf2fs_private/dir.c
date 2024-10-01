7cf75b8470e2bd841baed159105017fc6cbecfc9
Onanii Yui <177497261+yui-onanii@users.noreply.github.com>
2024-10-01 21:36:00 +0900

    remove name matching logic
    Signed-off-by: Onanii Yui <177497261+yui-onanii@users.noreply.github.com>

--- src/libf2fs_private/dir.c
+++ src/libf2fs_private/dir.c
@@ -3,8 +3,8 @@
 #include "fsck.h"
 #include "node.h"
 
-static /* struct f2fs_dir_entry * */ void find_target_dentry(const u8 *name,
-		unsigned int len, f2fs_hash_t namehash, int *max_slots,
+static /* struct f2fs_dir_entry * */ void find_target_dentry( /* const u8 *name,
+		unsigned int len, f2fs_hash_t namehash, */ int *max_slots,
 		struct f2fs_dentry_ptr *d)
 {
 	struct f2fs_dir_entry *de;
@@ -40,17 +40,17 @@ static /* struct f2fs_dir_entry * */ void find_target_dentry(const u8 *name,
 }
 
 static /* struct f2fs_dir_entry * */ void find_in_block(void *block,
-		const u8 *name, int len, f2fs_hash_t namehash,
+		//const u8 *name, int len, f2fs_hash_t namehash,
 		int *max_slots)
 {
 	struct f2fs_dentry_ptr d;
 
 	make_dentry_ptr(&d, NULL, block, 1);
-	/* return */ find_target_dentry(name, len, namehash, max_slots, &d);
+	/* return */ find_target_dentry(/* name, len, namehash, */ max_slots, &d);
 }
 
 static /* int */ void find_in_level(struct f2fs_sb_info *sbi, struct f2fs_node *dir,
-		unsigned int level, struct dentry *de)
+		unsigned int level /* , struct dentry *de */ )
 {
 	unsigned int nbucket, nblock;
 	unsigned int bidx, end_block;
@@ -59,18 +59,18 @@ static /* int */ void find_in_level(struct f2fs_sb_info *sbi, struct f2fs_node *
 	void *dentry_blk;
 	int max_slots = 214;
 	nid_t ino = le32_to_cpu(F2FS_NODE_FOOTER(dir)->ino);
-	f2fs_hash_t namehash;
+	//f2fs_hash_t namehash;
 	unsigned int dir_level = dir->i.i_dir_level;
 	int ret = 0;
 
-	namehash = f2fs_dentry_hash(get_encoding(sbi), IS_CASEFOLDED(&dir->i),
-					de->name, de->len);
+	/*namehash = f2fs_dentry_hash(get_encoding(sbi), IS_CASEFOLDED(&dir->i),
+					de->name, de->len);*/
 
 	nbucket = dir_buckets(level, dir_level);
 	nblock = bucket_blocks(level);
 
-	bidx = dir_block_index(level, dir_level, le32_to_cpu(namehash) % nbucket);
-	end_block = bidx + nblock;
+	bidx = dir_block_index(level, dir_level, /* le32_to_cpu(namehash) % nbucket */ 0);
+	end_block = bidx + /* nblock */ nbucket * nblock;
 
 	dentry_blk = calloc(F2FS_BLKSIZE, 1);
 	ASSERT(dentry_blk);
@@ -90,8 +90,8 @@ static /* int */ void find_in_level(struct f2fs_sb_info *sbi, struct f2fs_node *
 		ret = dev_read_block(dentry_blk, dn.data_blkaddr);
 		ASSERT(ret >= 0);
 
-		/* dentry = */ find_in_block(dentry_blk, de->name, de->len,
-						namehash, &max_slots);
+		/* dentry = */ find_in_block(dentry_blk, /* de->name, de->len,
+						namehash, */ &max_slots);
 		/*if (dentry) {
 			ret = 1;
 			de->ino = le32_to_cpu(dentry->ino);
@@ -107,30 +107,30 @@ static /* int */ void find_in_level(struct f2fs_sb_info *sbi, struct f2fs_node *
 }
 
 static /* int */ void f2fs_find_entry(struct f2fs_sb_info *sbi,
-				struct f2fs_node *dir, struct dentry *de)
+				struct f2fs_node *dir /* , struct dentry *de */ )
 {
 	unsigned int max_depth;
 	unsigned int level;
 
 	max_depth = le32_to_cpu(dir->i.i_current_depth);
 	for (level = 0; level < max_depth; level ++) {
-		/* if ( */ find_in_level(sbi, dir, level, de) // )
+		/* if ( */ find_in_level(sbi, dir, level /*, de */ ) // )
 			/* return 1 */ ;
 	}
 	//return 0;
 }
 
 /* og behavior: return ino if file exists, otherwise return 0 */
-/* nid_t */ void /* f2fs_lookup */ f2fs_listdir(struct f2fs_sb_info *sbi, struct f2fs_node *dir,
-				u8 *name, int len)
+/* nid_t */ void /* f2fs_lookup */ f2fs_listdir(struct f2fs_sb_info *sbi, struct f2fs_node *dir //,
+				/* u8 *name, int len */ )
 {
 	//int err;
-	struct dentry de = {
+	/*struct dentry de = {
 		.name = name,
 		.len = len,
-	};
+	};*/
 
-	/* err = */ f2fs_find_entry(sbi, dir, &de);
+	/* err = */ f2fs_find_entry(sbi, dir /*, &de */ );
 	/*if (err == 1)
 		return de.ino;
 	else
