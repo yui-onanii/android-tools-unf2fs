9e0459c9d80d0fdf94e782a425fb1e316558c90b
Onanii Yui <177497261+yui-onanii@users.noreply.github.com>
2024-10-02 00:43:44 +0900

    rename func
    Signed-off-by: Onanii Yui <177497261+yui-onanii@users.noreply.github.com>

--- src/libf2fs_private/f2fs_private.h
+++ src/libf2fs_private/f2fs_private.h
@@ -5,7 +5,7 @@
 #include "node.h"
 
 void
-f2fs_listdir (struct f2fs_sb_info *sbi,
-              struct f2fs_node *dir);
+f2fs_listdir_ (struct f2fs_sb_info *sbi,
+               struct f2fs_node *dir);
 
 #endif /* LIBF2FS_PRIVATE_H */
